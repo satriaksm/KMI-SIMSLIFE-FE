ce19950363c3a23015155184efdaadd53b9ca078
raffiatalla90 <raffiatalla9090@gmail.com>
2026-06-23 14:53:40 +0700

    fix(merchant): import watch in MerchantInfo.vue to fix ReferenceError

--- src/views/merchant/profile/MerchantInfo.vue
+++ src/views/merchant/profile/MerchantInfo.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, onMounted } from "vue";
+import { ref, computed, onMounted, watch } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import { useAuthStore } from "@/stores/auth";
 import { useToast } from "vue-toastification";
